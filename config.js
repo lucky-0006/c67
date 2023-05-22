c2f3b96509b3d9ab353bc85bddd16aa39f703978
lucky-0006 <129654319+lucky-0006@users.noreply.github.com>
2023-05-22 18:41:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
-};
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+   const firebaseConfig = {
+        apiKey: "AIzaSyA6r2bjyDAoPgb-42AMzXBUizw8dTyXuAk",
+        authDomain: "c-68-d94ca.firebaseapp.com",
+        projectId: "c-68-d94ca",
+        storageBucket: "c-68-d94ca.appspot.com",
+        messagingSenderId: "549896524297",
+        appId: "1:549896524297:web:3a21185177fc1041161ed8"
+      };
+    firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
